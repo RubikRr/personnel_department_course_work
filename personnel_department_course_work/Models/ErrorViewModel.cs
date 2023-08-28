433898f23c6970db40a9e9b52afec3725d102830
vova_chakalov <vova.chakalov.2003@gmail.com>
2023-08-28 23:35:36 +0300

    Add project files.

---
+++ personnel_department_course_work/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace personnel_department_course_work.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
