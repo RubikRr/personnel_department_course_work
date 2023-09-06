2add349a738b2a02558cd4740f5ea78ce7a709e2
vova_chakalov <vova.chakalov.2003@gmail.com>
2023-09-06 21:04:30 +0300

    добавил модель трудовой договор

---
+++ personnel_department_DB/Models/EmploymentContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace personnel_department_DB.Models
+{
+    [Table("EmploymentContracts")]
+    public class EmploymentContract
+    {
+        public Guid Id { get; set; }
+        public DateTime DateOfPreparation { get; set; }
+        public DateTime AcceptanceDate { get; set; }
+        public Decimal Salary { get; set; }
+        public Decimal Allowance { get; set; }
+
+        public Guid EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+        
+
+    }
+}
